b5591f5d1d3c950657435f88b80dca2c4ece0273
Bri997 <brian.musial@gmail.com>
2018-07-14 20:05:36 -0700

    yep

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import './contact.css';
+import "./contact.css";
 
 // The Contact component goes here.  It should be the default export.
+export default function Contact(props) {
+  return (
+    <section id={`contact-${props.index}`} className="contact">
+      <img
+        className="contact-photo"
+        src={props.photo}
+        alt={`${`props.name`}'s avatar1`}
+      />
+      <h2 className="contact-name">{props.name}</h2>
+      <adress className="contact-adress">{props.adress}</adress>
+    </section>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AddressBook from './components/address-book';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import AddressBook from "./components/address-book";
+import "./index.css";
 
-ReactDOM.render(
-  <AddressBook />,
-  document.getElementById('root')
-);
+ReactDOM.render(<AddressBook />, document.getElementById("root"));
